8f309cdff1344f65db63c4f32b2fe29dd864830c
John Hammond <jhammond@tacc.utexas.edu>
2012-02-28 12:54:14 -0600

    Use assert() in ASSERT().

--- trace.h
+++ trace.h
@@ -3,33 +3,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#undef NDEBUG
+#if ! DEBUG
+#define NDEBUG 1
+#endif
+#include <assert.h>
 
 #if DEBUG
-#define TRACE ERROR
-#define ERROR(fmt,args...) do {                    \
+#define TRACE(fmt,args...) do {                    \
     int _saved_errno = errno;                      \
-    fprintf(stderr, "%s:%d: "fmt, __func__, __LINE__, ##args);   \
+    fprintf(stderr, "%s: "fmt, __func__, ##args);  \
     errno = _saved_errno;                          \
   } while (0)
 #else
-static inline void TRACE(const char *fmt, ...) { } /* attribute() */
+__attribute__((format (printf, 1, 2)))
+static inline void TRACE(const char *fmt, ...) { }
+#endif
 
 #define ERROR(fmt,args...) do {                                         \
     int _saved_errno = errno;                                           \
     fprintf(stderr, "%s: "fmt, program_invocation_short_name, ##args);  \
     errno = _saved_errno;                                               \
   } while (0)
-#endif
 
 #define FATAL(fmt,args...) do { \
     ERROR(fmt, ##args);         \
     exit(1);                    \
   } while (0)
 
-#define ASSERT(c) do { \
-  if (!(c)) \
-    FATAL("failed assertion `%s'\n", #c); \
-  } while (0)
+#define ASSERT assert
 
 #define OOM() FATAL("out of memory\n");
 
