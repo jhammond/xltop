bfeb6ab6ccad5021665fa280fb86a00f1e8ec91c
John L. Hammond <jhammond@tacc.utexas.edu>
2012-11-08 16:39:15 -0600

    Remove unused container_of.h.

--- container_of.h
+++
@@ -1,7 +0,0 @@
-#include <stddef.h>
-#ifndef container_of
-#define container_of(ptr, type, member) ({			\
-      const typeof(((type *) 0)->member ) *__mptr = (ptr);	\
-      (type *) ((char *) __mptr - offsetof(type,member));	\
-    })
-#endif
